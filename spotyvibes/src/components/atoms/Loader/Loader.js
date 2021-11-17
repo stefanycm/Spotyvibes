b9edb2250e9bb7e81927b1bda4836b70bd86c01c
stefanycm <stefany.cardenas@pragma.com.co>
2021-11-17 08:37:39 -0500

    loader created

---
+++ spotyvibes/src/components/atoms/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './loader.scss';
+
+const Loader = () => {
+  return (
+    <div class="lds-ripple"><div></div><div></div></div>
+  );
+};
+
+export default Loader;
